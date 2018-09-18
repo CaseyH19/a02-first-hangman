dbbfb2175f3f10801a4883310739a4ef3f2a9a59
CaseyH19 <chittinger@smu.edu>
2018-09-18 16:43:25 -0500

    Implement tally file to show win or loss

--- hangman/lib/game/guess.ex
+++ hangman/lib/game/guess.ex
@@ -2,15 +2,28 @@ defmodule Hangman.Guess do
 
   #initializes a new Game
   def make_move(game, guess) do
-    g1 = repeat(Enum.member?(game.used, guess), game, guess)
+    g = repeat(Enum.member?(game.used, guess), game, guess)
+    g1 = Hangman.tally(g)
+    {g, g1}
   end
 
   def repeat(false, game, guess) do
-   %{game | used: [guess | game.used]}
+    g2 = %{game | used: [guess | game.used]}
+    good_bad(Enum.member?(g2.letters, guess), g2)
   end
 
   def repeat(true, game, _guess) do
     %{game | game_state: :already_used}
+  end
+
+
+
+  def good_bad(true, game) do
+    %{game | game_state: :good_guess}
+  end
+
+  def good_bad(false, game) do
+    %{game | turns_left: (game.turns_left-1), game_state: :bad_guess}
   end
 
 end
--- hangman/lib/game/tally.ex
+++ hangman/lib/game/tally.ex
@@ -5,12 +5,37 @@ defmodule Hangman.Tally do
     turns_left: 7,
     letters: [],
     used: [],
-    last_guess: '',
+    last_guess: ''
   )
 
   #returns the current game-card
   def tally(game) do
-    game
+    determine_tally(game, game.letters, [])
+  end
+
+  def determine_tally(game, [h1 | t1], final) do
+    determine_tally(game, t1, check(Enum.member?(game.used, h1), h1, final))
+  end
+
+  def determine_tally(game, [], final) do
+    cond do
+      (game.turns_left == 0) ->
+        %{game | letters: Enum.reverse(final), game_state: :lost}
+
+      (!Enum.member?(final, "_")) ->
+        %{game | letters: Enum.reverse(final), game_state: :won}
+
+      true ->
+        %{game | letters: Enum.reverse(final)}
+    end
+  end
+
+  def check(true, let, final) do
+    [let | final]
+  end
+
+  def check(false, _let, final) do
+    ["_" | final]
   end
 
 end
